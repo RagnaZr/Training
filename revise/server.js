a459fd19bb1d53a6621c75804722c4ce2077658d
thanhv2 <trieuvuong0168.com>
2019-05-31 15:23:42 +0700

    Remove the now ignored directory node_modules

--- revise/server.js
+++
@@ -1,6 +0,0 @@
-var app = require('./app');
-var port = process.env.PORT || 3000;
-
-var server = app.listen(port, function(){
-    console.log("Server listening on port" + port);
-});
--- revise/user/User.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var UsersData = new mongoose.Schema({
-    email: String,
-    password: String
-});
-mongoose.model('User', UsersData);
-
-module.exports= mongoose.model('User');
